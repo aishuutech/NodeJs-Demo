48fbfc91640e8c91b7b428148e711371d9ba68ad
Aiswarya K <aishuutech@gmail.com>
2022-11-11 15:21:21 +0530

    node assesment

---
+++ users/user.service.js
@@ -0,0 +1,109 @@
+﻿const config = require('config.js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+const User = db.User;
+const os = require('os');
+const { getClientIp } = require('request-ip');
+const Audit = db.Audit;
+module.exports = {
+    authenticate,
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete,
+    logout,
+    audits
+};
+
+async function authenticate({ username, password }) {
+    const user = await User.findOne({ username });
+    if (user && bcrypt.compareSync(password, user.hash)) {
+        const { hash, ...userWithoutHash } = user.toObject();
+        const token = jwt.sign({ sub: user.id }, config.secret);
+        const loginDate = new Date();
+        let audit = new Audit();
+        audit.userName = username;
+        audit.clientIp =JSON.stringify(os.networkInterfaces());
+        audit.login = loginDate;
+
+        await audit.save();
+        console.log({audit});       
+        return {
+            ...userWithoutHash,
+            token
+        };
+    }
+}
+
+async function getAll() {
+    return await User.find().select('-hash');
+}
+
+async function getById(id) {
+    return await User.findById(id).select('-hash');
+}
+
+async function create(userParam) {
+    // validate
+    if (await User.findOne({ username: userParam.username })) {
+        throw 'Username "' + userParam.username + '" is already taken';
+    }
+
+    const user = new User(userParam);
+
+    // hash password
+    if (userParam.password) {
+        user.hash = bcrypt.hashSync(userParam.password, 10);
+    }
+
+    // save user
+    await user.save();
+}
+
+async function update(id, userParam) {
+    const user = await User.findById(id);
+
+    // validate
+    if (!user) throw 'User not found';
+    if (user.username !== userParam.username && await User.findOne({ username: userParam.username })) {
+        throw 'Username "' + userParam.username + '" is already taken';
+    }
+
+    // hash password if it was entered
+    if (userParam.password) {
+        userParam.hash = bcrypt.hashSync(userParam.password, 10);
+    }
+
+    // copy userParam properties to user
+    Object.assign(user, userParam);
+
+    await user.save();
+}
+
+async function _delete(id) {
+    await User.findByIdAndRemove(id);
+}
+
+async function audits(id)
+    {
+        return await Audit.findById(id);
+    }
+
+async function logout(req,res){
+    const {username} = req.body;
+    console.log(username);
+    try{
+    const audit = await Audit.findOne({userName : username, logout : null});
+    console.log(audit)
+    const logoutDate = new Date();
+    audit.logout = logoutDate;
+    console.log(audit);
+    return await audit.save();
+    }
+    catch(err)
+    {
+        return res.status(401).json({message : "UnAuthorized Error"});
+    }
+}
